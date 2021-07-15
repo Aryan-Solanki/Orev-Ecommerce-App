b7077b5c5be5e1ea3ac66c50757603b3f2455150
Rishika Solanki <37494973+Aryan-Solanki@users.noreply.github.com>
2021-07-15 13:24:35 +0530

    ..

--- lib/screens/login_success/components/body.dart
+++ lib/screens/login_success/components/body.dart
@@ -4,35 +4,26 @@ import 'package:orev/components/default_button.dart';
 import 'package:orev/screens/home/home_screen.dart';
 import 'package:orev/size_config.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
+
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  @override
+  void initState() {
+    new Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, HomeScreen.routeName);
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(height: SizeConfig.screenHeight * 0.04),
-        Lottie.asset("assets/animation/success.json",
-            repeat: true), // Background Image !
-        SizedBox(height: SizeConfig.screenHeight * 0.08),
-        Text(
-          "Login Success",
-          style: TextStyle(
-            fontSize: getProportionateScreenWidth(30),
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-        ),
-        Spacer(),
-        SizedBox(
-          width: SizeConfig.screenWidth * 0.6,
-          child: DefaultButton(
-            text: "Back to home",
-            press: () {
-              Navigator.pushNamed(context, HomeScreen.routeName);
-            },
-          ),
-        ),
-        Spacer(),
-      ],
+    return Container(
+      child: Lottie.asset("assets/animation/success.json",
+          repeat: false),
     );
   }
 }
+
