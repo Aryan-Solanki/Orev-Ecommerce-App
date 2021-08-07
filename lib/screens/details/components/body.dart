16c45132176c4a1a123683b9e9396102159d54f2
Rishika Solanki <37494973+Aryan-Solanki@users.noreply.github.com>
2021-08-07 23:12:02 +0530

    ..

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:orev/components/default_button.dart';
@@ -14,6 +16,7 @@ import 'package:orev/screens/seemore/seemore.dart';
 import 'package:orev/screens/sign_in/sign_in_screen.dart';
 import 'package:orev/services/product_services.dart';
 import 'package:orev/size_config.dart';
+import 'package:paytm/paytm.dart';
 import 'package:search_choices/search_choices.dart';
 import 'package:slide_popup_dialog/slide_popup_dialog.dart' as slideDialog;
 import 'product_description.dart';
@@ -33,6 +36,82 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+
+
+  String payment_response;
+
+
+  String mid = "LsrZNj54827134067770";
+  String PAYTM_MERCHANT_KEY = "eMQRe_MdiSf0cuih";
+  String website = "DEFAULT";
+  bool testing = false;
+  bool loading = false;
+
+  void generateTxnToken() async {
+    setState(() {
+      loading = true;
+    });
+    String orderId = DateTime.now().millisecondsSinceEpoch.toString();
+
+    String callBackUrl = (testing
+        ? 'https://securegw-stage.paytm.in'
+        : 'https://securegw.paytm.in') +
+        '/theia/paytmCallback?ORDER_ID=' +
+        orderId;
+
+    //Host the Server Side Code on your Server and use your URL here. The following URL may or may not work. Because hosted on free server.
+    //Server Side code url: https://github.com/mrdishant/Paytm-Plugin-Server
+    var url = 'https://desolate-anchorage-29312.herokuapp.com/generateTxnToken';
+
+    var body = json.encode({
+      "mid": mid,
+      "key_secret": PAYTM_MERCHANT_KEY,
+      "website": website,
+      "orderId": orderId,
+      "amount": (widget.product.varients[selectedFoodVariants].price * quantity).toString(),
+      "callbackUrl": callBackUrl,
+      "custId": "122",
+      "testing": testing ? 0 : 1
+    });
+
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: body,
+        headers: {'Content-type': "application/json"},
+      );
+      print("Response is");
+      print(response.body);
+      String txnToken = response.body;
+      setState(() {
+        payment_response = txnToken;
+      });
+
+      var paytmResponse = Paytm.payWithPaytm(
+          mid, orderId, txnToken, (widget.product.varients[selectedFoodVariants].price * quantity).toString(), callBackUrl, testing);
+
+      paytmResponse.then((value) {
+        print(value);
+        setState(() {
+          loading = false;
+          print("Value is ");
+          print(value);
+          if (value['error']) {
+            payment_response = value['errorMessage'];
+          } else {
+            if (value['response'] != null) {
+              payment_response = value['response']['STATUS'];
+            }
+          }
+          payment_response += "\n" + value.toString();
+        });
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+
 
   // List<String> UserAddress = [
   //   "400-B,Pocket-N,Sarita Vihar ,New Delhi,110076",
@@ -541,7 +620,7 @@ class _BodyState extends State<Body> {
                         color: kPrimaryColor,
                         text: "Pay Online",
                         press: () {
-                          print("Payment Gateway");
+                          generateTxnToken();
                         },
                       ),
                       SizedBox(
@@ -659,8 +738,6 @@ class _BodyState extends State<Body> {
                                           color: kPrimaryColor2,
                                           text: "Buy Now",
                                           press: () {
-                                            // Navigator.pushNamed(
-                                            //     context, PaytmIntegeration.routeName);
                                             setState(() {});
                                             if (addressmap.isEmpty) {
 
