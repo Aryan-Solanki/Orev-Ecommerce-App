d25a81b04054aa43774fc9a8414b2bbbaae85c47
AKSHAT RASTOGI <akshat28vivek@hotmail.com>
2021-08-11 23:40:19 +0530

    cart ke andar ki details

--- lib/screens/cart/components/cart_card.dart
+++ lib/screens/cart/components/cart_card.dart
@@ -3,6 +3,7 @@ import 'package:orev/components/rounded_icon_btn.dart';
 import 'package:orev/models/Cart.dart';
 import 'package:orev/models/Product.dart';
 import 'package:orev/providers/auth_provider.dart';
+import 'package:orev/screens/details/details_screen.dart';
 import 'package:orev/services/product_services.dart';
 
 import '../../../constants.dart';
@@ -97,122 +98,129 @@ class _CartCardState extends State<CartCard> {
   @override
   Widget build(BuildContext context) {
     int quantity = widget.cart.numOfItem;
-    return Column(
-      children: [
-        Row(
-          children: [
-            SizedBox(
-              width: 88,
-              child: AspectRatio(
-                aspectRatio: 0.88,
-                child: Container(
-                  padding: EdgeInsets.all(getProportionateScreenWidth(10)),
-                  decoration: BoxDecoration(
-                    color: Color(0xFFF5F6F9),
-                    borderRadius: BorderRadius.circular(15),
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(
+        context,
+        DetailsScreen.routeName,
+        arguments: ProductDetailsArguments(product: widget.cart.product),
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                width: 88,
+                child: AspectRatio(
+                  aspectRatio: 0.88,
+                  child: Container(
+                    padding: EdgeInsets.all(getProportionateScreenWidth(10)),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF5F6F9),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Image.network(widget
+                        .cart.product.varients[selectedVarient].images[0]),
                   ),
-                  child: Image.network(
-                      widget.cart.product.varients[selectedVarient].images[0]),
                 ),
               ),
-            ),
-            SizedBox(width: 20),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              SizedBox(width: 20),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.cart.product.title,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: getProportionateScreenHeight(20)),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                    ),
+                    SizedBox(height: 3),
+                    Text(
+                      "${widget.cart.product.varients[selectedVarient].title}",
+                      style:
+                          TextStyle(fontSize: getProportionateScreenHeight(15)),
+                    ),
+                    Text.rich(
+                      TextSpan(
+                        text:
+                            "\₹${widget.cart.product.varients[selectedVarient].price}",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black,
+                            fontSize: getProportionateScreenHeight(20)),
+                        children: [
+                          TextSpan(
+                              text: " x${quantity}",
+                              style: Theme.of(context).textTheme.bodyText1),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: getProportionateScreenWidth(5)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
                 children: [
+                  RoundedIconBtn(
+                    width: 23.0,
+                    height: 23.0,
+                    colour: Color(0xFFB0B0B0).withOpacity(0.2),
+                    icon: Icons.remove,
+                    press: () {
+                      if (quantity != 1) {
+                        setState(() {
+                          --quantity;
+                          changeCartQty(quantity);
+                        });
+                      } else if (quantity == 1) {
+                        removeFromCart(widget.cart.varientNumber);
+                      }
+                    },
+                  ),
+                  SizedBox(width: getProportionateScreenWidth(7)),
                   Text(
-                    widget.cart.product.title,
+                    "x" + quantity.toString(),
                     style: TextStyle(
                         color: Colors.black,
                         fontSize: getProportionateScreenHeight(20)),
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
                   ),
-                  SizedBox(height: 3),
-                  Text(
-                    "${widget.cart.product.varients[selectedVarient].title}",
-                    style:
-                        TextStyle(fontSize: getProportionateScreenHeight(15)),
-                  ),
-                  Text.rich(
-                    TextSpan(
-                      text:
-                          "\₹${widget.cart.product.varients[selectedVarient].price}",
-                      style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          color: Colors.black,
-                          fontSize: getProportionateScreenHeight(20)),
-                      children: [
-                        TextSpan(
-                            text: " x${quantity}",
-                            style: Theme.of(context).textTheme.bodyText1),
-                      ],
-                    ),
+                  SizedBox(width: getProportionateScreenWidth(7)),
+                  RoundedIconBtn(
+                    width: 23.0,
+                    height: 23.0,
+                    colour: Color(0xFFB0B0B0).withOpacity(0.2),
+                    icon: Icons.add,
+                    showShadow: true,
+                    press: () {
+                      setState(() {
+                        ++quantity;
+                        changeCartQty(quantity);
+                      });
+                    },
                   ),
                 ],
               ),
-            )
-          ],
-        ),
-        SizedBox(height: getProportionateScreenWidth(5)),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                RoundedIconBtn(
-                  width: 23.0,
-                  height: 23.0,
-                  colour: Color(0xFFB0B0B0).withOpacity(0.2),
-                  icon: Icons.remove,
-                  press: () {
-                    if (quantity != 1) {
-                      setState(() {
-                        --quantity;
-                        changeCartQty(quantity);
-                      });
-                    } else if (quantity == 1) {
-                      removeFromCart(widget.cart.varientNumber);
-                    }
-                  },
-                ),
-                SizedBox(width: getProportionateScreenWidth(7)),
-                Text(
-                  "x" + quantity.toString(),
+              Text.rich(
+                TextSpan(
+                  text:
+                      "\₹${widget.cart.product.varients[selectedVarient].price * quantity}",
                   style: TextStyle(
-                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                      color: kPrimaryColor,
                       fontSize: getProportionateScreenHeight(20)),
                 ),
-                SizedBox(width: getProportionateScreenWidth(7)),
-                RoundedIconBtn(
-                  width: 23.0,
-                  height: 23.0,
-                  colour: Color(0xFFB0B0B0).withOpacity(0.2),
-                  icon: Icons.add,
-                  showShadow: true,
-                  press: () {
-                    setState(() {
-                      ++quantity;
-                      changeCartQty(quantity);
-                    });
-                  },
-                ),
-              ],
-            ),
-            Text.rich(
-              TextSpan(
-                text:
-                    "\₹${widget.cart.product.varients[selectedVarient].price * quantity}",
-                style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: kPrimaryColor,
-                    fontSize: getProportionateScreenHeight(20)),
               ),
-            ),
-          ],
-        ),
-      ],
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
