80d948685e4f27deb3350e3de4b644b1f5a92db0
AKSHAT RASTOGI <akshat28vivek@hotmail.com>
2021-08-20 13:14:32 +0530

    fhgfgh

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -493,23 +493,6 @@ class _BodyState extends State<Body> {
                                           SelectedAddress = addressmap[i];
                                           getFinalCost(SelectedAddress, true);
                                         });
-
-                                        // Second Time
-
-                                        distanceInMeters =
-                                            addressmap[i]["distanceInMeters"];
-
-                                        if ((distanceInMeters / 1000) <
-                                            sellingdistance) {
-                                          deliverable = true;
-                                        } else {
-                                          deliverable = false;
-                                        }
-                                        _radioSelected = i;
-                                        setState(() {
-                                          SelectedAddress = addressmap[i];
-                                          getFinalCost(SelectedAddress, true);
-                                        });
                                       },
                                       child: Container(
                                           width:
