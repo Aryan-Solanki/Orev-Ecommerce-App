46cf5813cf96ede80d569cc187d5ab43e48e9629
Rishika Solanki <37494973+Aryan-Solanki@users.noreply.github.com>
2021-09-22 12:00:09 +0530

    ..

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:orev/components/coustom_bottom_nav_bar.dart';
+import 'package:orev/components/updateavailablescreen.dart';
 import 'package:orev/enums.dart';
 import 'package:orev/services/product_services.dart';
 import 'package:package_info_plus/package_info_plus.dart';
@@ -35,8 +36,16 @@ class _HomeScreenState extends State<HomeScreen> {
     maintenance = check["maintenance"];
 
     if (updateAvailable) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => UpdateAvailable(value: "Download Now",bottomNavigation: false,)),
+      );
       print("Push to Update Screen");
     } else if (maintenance) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => UpdateAvailable(value: "Come Back Soon",bottomNavigation: false,)),
+      );
       print("Push to Maintenance Screen");
     }
   }
