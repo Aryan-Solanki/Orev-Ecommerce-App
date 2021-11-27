7bdd7cccd81114250ce2b8832a70273824e1c2ba
AKSHAT RASTOGI <akshat28vivek@hotmail.com>
2021-11-27 20:31:02 +0530

    multiple payment success responsive

--- lib/screens/multiple_payment_success/multiple_payment_success.dart
+++ lib/screens/multiple_payment_success/multiple_payment_success.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:orev/constants.dart';
 import 'package:orev/models/Order.dart';
 import 'package:orev/screens/home/home_screen.dart';
+import 'package:orev/screens/multiple_payment_success/components/MultiplePaymentSuccessMobile.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
+import 'components/MultiplePaymentSuccessDesktop.dart';
 import 'components/body.dart';
 
 class MultiplePaymentSuccess extends StatefulWidget {
@@ -36,11 +39,23 @@ class _MultiplePaymentSuccessState extends State<MultiplePaymentSuccess> {
                 ? "Payment Success"
                 : "Payment Failure"),
           ),
-          body: Body(
-              transaction: widget.transaction_success,
-              order: widget.order,
-              cod: widget.cod,
-              orderTotal: widget.orderTotal),
+          body: ScreenTypeLayout(
+            mobile: MultiplePaymentSuccessMobile(
+                transaction: widget.transaction_success,
+                order: widget.order,
+                cod: widget.cod,
+                orderTotal: widget.orderTotal),
+            tablet: MultiplePaymentSuccessDesktop(
+                transaction: widget.transaction_success,
+                order: widget.order,
+                cod: widget.cod,
+                orderTotal: widget.orderTotal),
+            desktop: MultiplePaymentSuccessDesktop(
+                transaction: widget.transaction_success,
+                order: widget.order,
+                cod: widget.cod,
+                orderTotal: widget.orderTotal),
+          ),
         ),
       ),
     );
