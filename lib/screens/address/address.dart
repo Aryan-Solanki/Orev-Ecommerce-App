1a4d0dcf5740844b87c07173a0b244563364ac93
AKSHAT RASTOGI <akshat28vivek@hotmail.com>
2021-11-27 19:18:00 +0530

    Update address.dart

--- lib/screens/address/address.dart
+++ lib/screens/address/address.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
-
+import 'package:orev/screens/address/components/AddressPageDesktop.dart';
+import 'package:orev/screens/address/components/AddressPageMobile.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 import 'components/body.dart';
 
 class Address extends StatelessWidget {
   static String routeName = "/Address";
+
+  const Address({
+    Key key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Address Form"),
       ),
-      body: Body(),
+      body: ScreenTypeLayout(
+        mobile: AddressPageMobile(),
+        tablet: AddressPageDesktop(),
+        desktop: AddressPageDesktop(),
+      ),
     );
   }
 }
